3c0ac52207be30f4758043ca1d56691187c70f67
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-04-28 17:47:31 +0100

    add actual google TTS API code

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 import mistune
 import sys
+import argparse
 
 class Renderer(mistune.Renderer):
     def header(self,text,level,raw=None):
@@ -16,5 +17,38 @@ class Renderer(mistune.Renderer):
 renderer = Renderer()
 markdown = mistune.Markdown(renderer=renderer)
 out_text = markdown("\n".join(sys.stdin.readlines()))
+ssml = f"<speak>{out_text}</speak>"
 
-print(f"<speak>{out_text}</speak>")
+print(ssml)
+
+def get_ogg(ssml):
+    """
+    Get the MP3 from google cloud TTS API
+    """
+    from google.cloud import texttospeech
+    client = texttospeech.TextToSpeechClient()
+
+    input_text = texttospeech.types.SynthesisInput(ssml=ssml)
+
+    # Note: the voice can also be specified by name.
+    # Names of voices can be retrieved with client.list_voices().
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        name="en-US-Wavenet-F"
+        )
+
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.OGG_OPUS)
+
+    response = client.synthesize_speech(input_text, voice, audio_config)
+
+    # The response's audio_content is binary.
+    with open('output.ogg', 'wb') as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.ogg"')
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--get_ogg", action='store_true', default=False)
+args = parser.parse_args()
+if args.get_ogg:
+    get_ogg(ssml)
